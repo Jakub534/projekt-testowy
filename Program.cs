25ffcdd17b5a63b8a85226d4eec2d94af91ef3f4
Jakub47 <jakub7249@gmail.com>
2017-08-25 11:19:48 +0200

    Add files via upload
    dasdas

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoIdditiesHere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            NoOdds(array);
+            foreach(int x in array)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadKey();
+        }
+
+        //Write a function will return an array of non-odd values
+        public static int[] NoOdds(int[] values)
+        {
+           //the simplest way 1) Create a new array
+            int[] newOne = new int[values.Length];
+            for(int i = 0;i<values.Length;i++)
+            {
+                if(!(values[i] % 2==0 ))
+                {
+                    newOne[i] = values[i];
+                }
+
+            }
+            return newOne;
+        }
+    }
+}
